61ba4330863799ab45bba1a982dea5284e7353c3
Philipp Weissensteiner <mail@philippweissensteiner.com>
2017-01-11 15:48:26 +0100

    Avoid the content script being injected twice

--- extension/content-script.js
+++ extension/content-script.js
@@ -7,21 +7,25 @@
 //                   | window |                  | port |
 // webkitGetUserMedia|<------ |window.postMessage|<-----| port.postMessage
 //
+(function() {
+  if (window.contentScriptHasRun) return;
+  window.contentScriptHasRun = true;
 
-var port = chrome.runtime.connect(chrome.runtime.id);
+  var port = chrome.runtime.connect(chrome.runtime.id);
 
-port.onMessage.addListener(function(msg) {
-	window.postMessage(msg, '*');
-});
+  port.onMessage.addListener(function(msg) {
+  	window.postMessage(msg, '*');
+  });
 
-window.addEventListener('message', function(event) {
-	// We only accept messages from ourselves
-	if (event.source != window) return;
+  window.addEventListener('message', function(event) {
+  	// We only accept messages from ourselves
+  	if (event.source != window) return;
 
-	if (event.data.type && ((event.data.type === 'SS_UI_REQUEST') ||
-							(event.data.type === 'SS_UI_CANCEL'))) {
-		port.postMessage(event.data);
-	}
-}, false);
+  	if (event.data.type && ((event.data.type === 'SS_UI_REQUEST') ||
+  							(event.data.type === 'SS_UI_CANCEL'))) {
+  		port.postMessage(event.data);
+  	}
+  }, false);
 
-window.postMessage({ type: 'SS_PING', text: 'start' }, '*');
+  window.postMessage({ type: 'SS_PING', text: 'start' }, '*');
+})();
